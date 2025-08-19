c07a8b4b10aebd6564e7e45a5bd94570d2f13861
Spark16x <spark2009971@gmail.com>
2025-08-19 19:51:29 +0530

    update

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
+import Link from 'next/link';
 
 export default function Home() {
-  return (<div>Spark</div>);
+  return (
+  <>
+    <header>
+      <Link href="#">Sprak</Link>
+      
+    </header>
+  </>);
 }
