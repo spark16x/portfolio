0f9678290eafb09c1ffb842ee355b27f95a1c169
Spark16x <spark2009971@gmail.com>
2025-08-19 21:43:46 +0530

    update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
     <header className="w-full h-[50px] flex justify-between content-center bg-zinc-800">
-      <Link href="#" className="text-red-400 w-sm" >Sprak</Link>
+      <Link href="#" className="text-red-600 w-sm" >Sprak</Link>
       <nav className="w-1/2 flex justify-around">
       <Link href="#" className="">Home</Link>
       <Link href="#" className="">About</Link>
