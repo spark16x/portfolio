ca021dd86214330b1433393e90a159e588861174
Spark16x <spark2009971@gmail.com>
2025-08-19 22:02:35 +0530

    update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -7,10 +7,10 @@ export default function Home() {
     <header className="w-full h-[50px] flex justify-between items-center bg-zinc-800">
       <Link href="#" className="p-[10px] text-red-600 text-lg" >Sprak</Link>
       <nav className="w-1/2 flex justify-around">
-      <Link href="#" className="hover:text-red-600">Home</Link>
-      <Link href="#" className="hover:text-red-600">About</Link>
-      <Link href="#" className="hover:text-red-600">Skill</Link>
-      <Link href="#" className="hover:text-red-600">Contact</Link>
+      <Link href="#" className="hover:text-red-600 hover:underline">Home</Link>
+      <Link href="#" className="hover:text-red-600 hover:underline">About</Link>
+      <Link href="#" className="hover:text-red-600 hover:underline">Skill</Link>
+      <Link href="#" className="hover:text-red-600 hover:underline">Contact</Link>
       </nav>
     </header>
   </>);
