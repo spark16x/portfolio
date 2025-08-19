cece41e80f1bcf4a6ed0f1f34749a1a7d8949802
Spark16x <spark2009971@gmail.com>
2025-08-19 21:34:57 +0530

    update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <>
-    <header className="w-full h-[50px] flex justify-between justify-around bg-zinc-800">
-      <Link href="#" className="text-red-500 w-sm" >Sprak</Link>
+    <header className="w-full h-[50px] flex justify-between content-center bg-zinc-800">
+      <Link href="#" className="text-red-400 w-sm" >Sprak</Link>
       <nav className=" flex justify-around">
       <Link href="#" className="">Home</Link>
       <Link href="#" className="">About</Link>
