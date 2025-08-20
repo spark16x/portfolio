05258c4bdf9c6c0f4fc4f0a03dda60f778883c65
Spark16x <spark2009971@gmail.com>
2025-08-20 20:11:50 +0530

    update

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed3 = new Typed('.typing', {
+    strings: ['Web developer','Frontend developer','Backend developer'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    smartBackspace: true, // this is a default
+    loop: true
+  });
+    
