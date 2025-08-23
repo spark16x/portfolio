6118a5e7c0f927a1e05f09b61d2fd9b0ff8887c0
Spark16x <spark2009971@gmail.com>
2025-08-23 14:31:48 +0530

    update

--- main.js
+++ main.js
@@ -5,6 +5,23 @@ var typed3 = new Typed('.typing', {
   smartBackspace: true, // this is a default
   loop: true
 });
+let menuBtn=document.querySelector("#menu-b")
+let menu=document.querySelector("#menu")
+
+let mOpen=false;
+menuBtn.addEventListener('click',(ev)=>{
+  if (!mOpen) {
+    gsap.to('#Vector', { duration: 0.5, morphSVG:'M20 20L4 4.00003M20 4L4.00002 20'  });
+    gsap.to('#menu',{duration: 0.5, x:'-100%'})
+    mOpen=true;
+  } else {
+    gsap.to('#Vector', { duration: 0.5, morphSVG:'M5 15H19M5 9H19'  });
+    gsap.to('#menu',{duration: 0.5, x:'100%'})
+
+    mOpen=false;
+  }
+  
+})
 
 
 
