65e235f71bc9ee3d50e1a98ec4b2c18cff45fbee
Spark16x <spark2009971@gmail.com>
2025-08-23 15:13:34 +0530

    update

--- main.js
+++ main.js
@@ -1,28 +1,39 @@
-var typed3 = new Typed('.typing', {
-  strings: ['Web developer', 'Frontend developer', 'Backend developer'],
-  typeSpeed: 50,
-  backSpeed: 50,
-  smartBackspace: true, // this is a default
-  loop: true
+const typingText = Typify('.typing', {
+  text: ['Web developer', 'Frontend developer', 'Backend developer'],
+  delay: 100,
+  loop: true,
+  cursor: true,
+  stringDelay: 1000
 });
-let menuBtn=document.querySelector("#menu-b")
-let menu=document.querySelector("#menu")
 
-let mOpen=false;
-menuBtn.addEventListener('click',(ev)=>{
-  if (!mOpen) {
-    gsap.to('#Vector', { duration: 0.5, morphSVG:'M20 20L4 4.00003M20 4L4.00002 20'  });
-    gsap.to('#menu',{duration: 0.5, x:'-100%'})
-    mOpen=true;
-  } else {
-    gsap.to('#Vector', { duration: 0.5, morphSVG:'M5 15H19M5 9H19'  });
-    gsap.to('#menu',{duration: 0.5, x:'100%'})
+let menuBtn = document.querySelector("#menu-b")
+let a = document.querySelectorAll("#menu a");
+let isMobile = window.innerWidth < 640;
+let menuItems = ['Home', 'About', 'Skill', 'Contact']
+let mOpen = false;
 
-    mOpen=false;
-  }
+if (isMobile) {
+  menuBtn.addEventListener('click', (ev) => {
+    if (!mOpen) {
+      gsap.to('#Vector', { duration: 0.5, morphSVG: 'M20 20L4 4.00003M20 4L4.00002 20' });
+      gsap.to('#menu', { duration: 0.5, x: '-100%' })
+      mOpen = true;
+    } else {
+      gsap.to('#Vector', { duration: 0.5, morphSVG: 'M5 15H19M5 9H19' });
+      gsap.to('#menu', { duration: 0.5, x: '100%' })
+      
+      mOpen = false;
+    }
+    
+  })
   
-})
-
-
-
-
+  a.forEach((e, i) => {
+    
+    e.addEventListener('pointerenter', (ev) => {
+      gsap.to(e, { duration: 1.5, scrambleText: { 
+        text: menuItems[i],
+      } })
+    })
+  }); //or customize things:
+  
+}
