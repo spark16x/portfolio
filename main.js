a6171e61d67e3600a7d52c0ad0a24eba654ea897
Spark16x <spark2009971@gmail.com>
2025-08-31 15:53:15 +0530

    update

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-const typingText = Typify('.typing', {
-  text: ['Web developer', 'Frontend developer', 'Backend developer'],
-  delay: 100,
-  loop: true,
-  cursor: true,
-  stringDelay: 1000
-});
-
-
 let menuBtn = document.querySelector("#menu-b")
 let a = document.querySelectorAll("#menu a");
 let isMobile = window.innerWidth < 640;
 let menuItems = ['Home', 'About', 'Skills', 'Contact']
 let mOpen = false;
 
+window.addEventListener('load',()=>{
+  console.log('loaded')
+  gsap.to('#loading',{opacity:0,duration:1,delay:1})
+  setTimeout(()=>document.querySelector('#loading').classList.add('hidden'),2000)
+  
+  
+
+})
+
 if (isMobile) {
   menuBtn.addEventListener('click', (ev) => {
     if (!mOpen) {
       gsap.to('#Vector', { duration: 0.5, morphSVG: 'M20 20L4 4.00003M20 4L4.00002 20' });
-      gsap.to('#menu', { duration: 0.5, x: '-100%' })
+      gsap.to('#menu', { duration: 0.5, x: '-100%'  ,ease: "power4.Out"})
       mOpen = true;
     } else {
       gsap.to('#Vector', { duration: 0.5, morphSVG: 'M5 15H19M5 9H19' });
-      gsap.to('#menu', { duration: 0.5, x: '100%' })
+      gsap.to('#menu', { duration: 0.5, x: '100%' ,ease: "power4.Out"})
       
       mOpen = false;
     }
@@ -48,3 +48,10 @@ if (isMobile) {
   
   
 }
+const typingText = Typify('.typing', {
+  text: ['Web developer', 'Frontend developer', 'Backend developer'],
+  delay: 100,
+  loop: true,
+  cursor: true,
+  stringDelay: 1000
+});
